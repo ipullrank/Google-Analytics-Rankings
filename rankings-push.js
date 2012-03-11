c1ccf8435cbf29a8a683d0eb6edee3060010cec8
ipullrank <mike@ipullrank.com>
2012-03-11 19:53:55 -0300

    Updated 3/11/2012 to change the scope in GA, check for any Google property (not just google.com) and return ranking for dynamic messaging applications.

--- rankings-push.js
+++ rankings-push.js
@@ -12,6 +12,10 @@ For information on the applications of this
 see John Doherty's post on SEOmoz:
 http://www.seomoz.org/blog/gettings-rankings-into-ga-using-custom-variables
 
+Updated 3/11/2012 to change the scope in GA, check for any
+Google property (not just google.com) and return ranking
+for dynamic messaging applications.
+
 */
 
 function rankingsPush()
@@ -19,7 +23,7 @@ function rankingsPush()
   var url = String(document.referrer);
 
 	// confirm they came from G
-	if (url.indexOf ("google.com") !=-1)
+	if (url.indexOf ("google.") !=-1)
 	{	
 
 		var urlVars = {};
@@ -29,7 +33,12 @@ function rankingsPush()
 		});
 
 		// Push to GA Custom Variables
-		_gaq.push(['_setCustomVar', '1', 'Rankings', urlVars["cd"], 1]);
+		_gaq.push(['_setCustomVar', '1', 'Rankings', urlVars["cd"], 2]);
+		
+		// Return the ranking in case 
+		// you dynamic targeting based on ranking
+		
+		return urlVars["cd"];
 
 	}
 
